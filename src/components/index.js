be84cade296ce1cc8660b76a03f151dd58cbfb48
priyanshi dasondhi <buntydarbar111@gmail.com>
2025-08-01 05:50:30 -0500

    fixed footer issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 
 import Select from "./Select";
 import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
+import Footer from "./footer/footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
